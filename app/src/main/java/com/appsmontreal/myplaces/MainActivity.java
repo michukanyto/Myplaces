9dae6a00bc3a9fc39560d34873315ffd0f955ddf
Marlon <escobar.marlon@gmail.com>
2019-08-18 19:51:52 -0400

    launchMapActivity implemented

--- app/src/main/java/com/appsmontreal/myplaces/MainActivity.java
+++ app/src/main/java/com/appsmontreal/myplaces/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,8 +37,9 @@ public class MainActivity extends AppCompatActivity implements MemorablePlace {
         goMapsImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intent = new Intent(view.getContext(),MapsActivity.class);
-                startActivityForResult(intent,1);
+//                intent = new Intent(view.getContext(),MapsActivity.class);
+//                startActivityForResult(intent,1);
+                launchMapActivity();
             }
         });
     }
@@ -48,29 +50,28 @@ public class MainActivity extends AppCompatActivity implements MemorablePlace {
         Log.i("------------>", "We're here onRestart ------->");
         arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,Place.places);
         placesListView.setAdapter(arrayAdapter);
+        placesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                launchMapActivity();
+            }
+        });
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        Log.i("\n\n------------>", "We're here" + "\n\n");
-//        if (requestCode == 1) {
-//            if (requestCode == RESULT_OK) {
-//               Place place = (Place) data.getSerializableExtra("NEW_POINT");
-//                Log.i("\n\n------------>", "We're here" + place + "\n\n");
-//
-////                arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,Place.places);
-////                placesListView.setAdapter(arrayAdapter);
-//
-//            } else if (requestCode == RESULT_CANCELED) {
-//                Log.i("\n\n------------>", "Any place selected" + "\n\n");
-//            }
-//
-//        }
-//    }
+    private void launchMapActivity() {
+        intent = new Intent(this,MapsActivity.class);
+        startActivityForResult(intent,1);
+    }
+
+
 
     @Override
     public void getNewMemorablePlace(Place place) {
         Log.i("\n\n------------>", "interface method" + place + "\n\n");
     }
+
+//    @Override
+//    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//    }
 }
